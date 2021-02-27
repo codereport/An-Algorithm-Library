a884826266d914937c23d68625f9a073565cf13a
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-02-27 17:00:18 -0500

    Create algorithm.hpp

---
+++ algorithm.hpp
@@ -0,0 +1,25 @@
+
+#include <tuple>
+
+namespace aal {
+namespace var {
+
+    template <typename I, typename... Is, typename P>
+    [[nodiscard]] constexpr auto 
+    find(P pred, I f, I l, Is... fs) {
+        while (f != l) {
+            if (pred(*f, *fs...)) break;
+            ++f, (++fs,...);
+        }
+        return std::tuple{f, fs...};
+    }
+
+    template <typename I, typename... Is, typename P>
+    [[nodiscard]] constexpr auto 
+    found(P pred, I f, I l, Is... fs) {
+        auto const t = find(pred, f, l, fs...);
+        return std::get<0>(t) != l;
+    }
+
+}
+}
