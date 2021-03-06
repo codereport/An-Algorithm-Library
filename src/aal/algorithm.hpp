05a398a2b161071e2371d08c9109af2517821c29
Conor Hoekstra <codereport@outlook.com>
2021-03-05 19:10:03 -0500

    Add aal::var::transform

--- src/aal/algorithm.hpp
+++ src/aal/algorithm.hpp
@@ -4,22 +4,32 @@
 namespace aal {
 namespace var {
 
-    template <typename I, typename... Is, typename P>
-    [[nodiscard]] constexpr auto 
-    find(P pred, I f, I l, Is... fs) {
-        while (f != l) {
-            if (pred(*f, *fs...)) break;
-            ++f, (++fs,...);
-        }
-        return std::tuple{f, fs...};
+template <typename I, typename... Is, typename P>
+[[nodiscard]] constexpr auto
+find(P pred, I f, I l, Is... fs) {
+    while (f != l) {
+        if (pred(*f, *fs...)) break;
+        ++f, (++fs, ...);
     }
+    return std::tuple{f, fs...};
+}
 
-    template <typename I, typename... Is, typename P>
-    [[nodiscard]] constexpr auto 
-    found(P pred, I f, I l, Is... fs) {
-        auto const t = find(pred, f, l, fs...);
-        return std::get<0>(t) != l;
+template <typename I, typename... Is, typename P>
+[[nodiscard]] constexpr auto
+found(P pred, I f, I l, Is... fs) {
+    auto const t = find(pred, f, l, fs...);
+    return std::get<0>(t) != l;
+}
+
+template <typename I, typename... Is, typename O, typename Op>
+constexpr auto
+transform(Op op, O o, I f, I l, Is... fs) {
+    while (f != l) {
+        *o = op(*f, *fs...);
+        ++o, ++f, (++fs, ...);
     }
+    return o;
+}
 
-}
-}
+}  // namespace var
+}  // namespace aal
